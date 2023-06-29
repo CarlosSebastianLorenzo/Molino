0312444b996c89d53481bf62f101509def6945fa
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-06-28 23:12:43 -0300

    componentes saldos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AltaDeCuentaComponent } from './componentes/alta-de-cuenta/alta-de-cuenta.component';
 import { LibroDeMovimientosComponent } from './componentes/libro-de-movimientos/libro-de-movimientos.component';
+import { SaldosComponent } from './componentes/saldos/saldos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AltaDeCuentaComponent,
-    LibroDeMovimientosComponent
+    LibroDeMovimientosComponent,
+    SaldosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/saldos/saldos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saldos',
+  templateUrl: './saldos.component.html',
+  styleUrls: ['./saldos.component.css']
+})
+export class SaldosComponent {
+
+}
