a444bde81b4ecee1989bc53e2947222c3ea86abc
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-06-29 22:57:26 -0300

    table Saldos

--- src/app/componentes/saldos/saldos.component.ts
+++ src/app/componentes/saldos/saldos.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./saldos.component.css']
 })
 export class SaldosComponent {
-
+  cuentas = [
+    {
+      Fecha: "01/01/2015",
+      Cliente: "Charreun Flavio",
+      Moneda: "Ch Prov BsAs",
+      Nro: "0025",
+      FechaDePago: "01/01/2015",
+      Monto: 25000,
+    },
+    {
+      Fecha: "01/01/2015",
+      Cliente: "Charreun Flavio",
+      Moneda: "Ch Prov BsAs",
+      Nro: "0025",
+      FechaDePago: "01/01/2015",
+      Monto: 25000,
+    }
+  ]
 }
